2ebd281fb51c3cf3d2a632a9c3d5bb67639bfcb5
meferyilmaz <99425043+meferyilmaz@users.noreply.github.com>
2022-04-20 18:35:03 +0300

    added a folder called power into the list a

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,4 +8,13 @@ public class HelloWorld {
     // Awesome work 3 is done and ready to be committed
     // and ready to be pushed to remote repository
 
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //
+    }
+
+
+
+
+
 }
